099f6704419384725d87ff148242b02b9f59881d
sandeepshe <sandeep.shenoy@citrix.com>
2020-09-13 09:55:16 -0400

    added linting to server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 
-function index(req, res) {
-    res.render(
-        path.join(__dirname, '..', 'dist', 'client', 'index'));
+function index (req, res) {
+  res.render(
+    path.join(__dirname, '..', 'dist', 'client', 'index'));
 }
 
-module.exports = function() {
-    var router = express.Router();
-    router.get('*', index);
-    return router;
+module.exports = function () {
+  var router = express.Router();
+  router.get('*', index);
+  return router;
 };
